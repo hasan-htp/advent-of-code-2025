fb5a16930ca8fabc1364e4bbe21176dd20b90ebe
hasan <hasan.hatipoglu.a@gmail.com>
2025-12-03 00:10:50 +0100

    day_2: cpp, part1 and part2 solution

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -10,14 +10,16 @@ struct ID
 {
     ID(const std::string& pair){
         size_t pos = pair.find('-');
-        first = pair.substr(0,pos);
-        last = pair.substr(pos+1, pair.length());
-        // std::cout<<"first: "<< first<<std::endl;
-        // std::cout<<"last: "<< last<<std::endl;
+        auto first_str = pair.substr(0,pos);
+        auto last_str = pair.substr(pos+1, pair.length());
+
+        first = std::stoll(first_str);
+        last = std::stoll(last_str);
 
     }
-    std::string first;
-    std::string  last;
+
+    uint64_t first;
+    uint64_t last;
 };
 
 std::vector<ID> id_reader(const std::string& path){
@@ -42,8 +44,59 @@ std::vector<ID> id_reader(const std::string& path){
     return ids;
 }
 
-std::string solution(const std::vector<ID>& id){
-    return "";
+namespace part1
+{
+
+bool is_invalid(uint64_t input) {
+    auto s = std::to_string(input);
+    const auto size = s.size();
+    if (size% 2 != 0){
+        return false;  
+    } 
+    
+    auto half = size / 2;
+    return s.compare(0, half, s, half, half) == 0;
+}
+
+uint64_t solution(const std::vector<ID>& ids){
+    uint64_t res = 0;
+    
+    for (const auto& id: ids){
+        for(uint64_t num=id.first ; num<= id.last; num++ ){
+            if(is_invalid(num)){
+                res+=num;
+            }
+        }
+    }
+    return res;
+}
+}
+
+namespace part2
+{
+
+// example input is 2424 -> ss is 24242424 mid is 42424, 2424 is in 424242 -> return true
+// example input is 599 -> ss is 599599 mid is 9959, 599 is NOT in 9959 -> return false
+
+bool is_invalid(uint64_t input) {
+    auto s = std::to_string(input);
+    auto ss = s + s;       
+    auto mid = ss.substr(1, ss.size() - 2);
+    return mid.find(s) != std::string::npos;
+}
+
+uint64_t solution(const std::vector<ID>& ids){
+    uint64_t res = 0;
+    
+    for (const auto& id: ids){
+        for(uint64_t num=id.first ; num<= id.last; num++ ){
+            if(is_invalid(num)){
+                res+=num;
+            }
+        }
+    }
+    return res;
+}
 }
 
 
@@ -52,11 +105,22 @@ int main(){
     auto default_input = id_reader("day_2/default_input.txt");
     auto my_input = id_reader("day_2/my_input.txt");
 
-    auto sol1 = solution(default_input);
-    auto sol2 = solution(my_input);
+    {
+        auto sol1 = part1::solution(default_input);
+        auto sol2 = part1::solution(my_input);
 
-    std::cout<<"default solution: "<<sol1<<std::endl;
-    std::cout<<"mu solution: "<<sol2<<std::endl;
+        std::cout<<"default solution: "<<sol1<<std::endl;
+        std::cout<<"my solution: "<<sol2<<std::endl;
+    }
+
+    {
+        auto sol1 = part2::solution(default_input);
+        auto sol2 = part2::solution(my_input);
+
+        std::cout<<"default solution: "<<sol1<<std::endl;
+        std::cout<<"my solution: "<<sol2<<std::endl;
+    }
+
 
     return 0;
 }
