a88b4aeba4172b9081c7e9c12dfd44c525195f4e
hasan <hasan.hatipoglu.a@gmail.com>
2025-12-03 22:16:23 +0100

    day_3: cpp, part2 solution

--- day_3/main.cpp
+++ day_3/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
@@ -50,6 +51,27 @@ int solution(const banks &banks) {
 }
 } // namespace part1
 
+namespace part2 {
+
+uint64_t solution(const banks &banks) {
+    uint64_t sum = 0;
+    for (const auto &bank : banks) {
+
+        uint64_t num = 0;
+        auto digit = bank.begin();
+        for (int i = 11; i >= 0; i--) {
+            digit = std::max_element(digit, bank.end() - i);
+            num += (*digit) * std::pow(10UL, i);
+
+            digit++;
+        }
+
+        sum += num;
+    }
+    return sum;
+}
+} // namespace part2
+
 int main(int argc, char **argv) {
 
     if (argc != 2) {
@@ -61,5 +83,7 @@ int main(int argc, char **argv) {
 
     auto input = battery_banks_reader(path);
     std::cout << part1::solution(input) << std::endl;
+    std::cout << part2::solution(input) << std::endl;
+
     return 0;
 }
